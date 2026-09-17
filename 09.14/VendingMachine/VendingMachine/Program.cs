6951e90df25dc9e9189cc0f18d9561c069335359
vendddel <plaveczvendi@gmail.com>
2026-09-17 12:08:56 +0200

    class kesz

--- 09.14/VendingMachine/VendingMachine/Program.cs
+++ 09.14/VendingMachine/VendingMachine/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace VendingMachine
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
     }
+
+    public class Product
+    {
+        private string _code {  get; set; }
+        private string _name { get; set; }
+        private int _price { get; set; }
+        private int _stock { get; set; }
+        private ProductCategory _category { get; set; }
+
+        
+    }
+
 }
