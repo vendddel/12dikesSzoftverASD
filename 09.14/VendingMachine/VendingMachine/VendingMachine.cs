bf4603ab113853f9934591b4e359cbd5b41d6ec2
vendddel <plaveczvendi@gmail.com>
2026-09-21 14:34:06 +0200

    19-es feladatig kesz

--- 09.14/VendingMachine/VendingMachine/VendingMachine.cs
+++ 09.14/VendingMachine/VendingMachine/VendingMachine.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -39,7 +41,75 @@ namespace VendingMachine
                     break;
 
             }
-               
         }
+
+        public void LoadProducts(string filename)
+        {
+            List<Product> _products = new List<Product>();
+      
+            string[] lines = File.ReadAllLines(filename);
+            foreach (string line in lines)
+            {
+                string[] parts = lines[0].Split(';');
+                int price = int.Parse(parts[2]);
+                int stock = int.Parse(parts[3]);
+                ProductCategory category = ParseCategory(parts[4]);
+
+               _products.Add(new Product(parts[0], parts[1], price, stock, category));
+            }
+          
+        }
+
+        public VendingMachine(string filename)
+        { 
+            _balance = 0;
+            _income = 0;
+            LoadProducts(filename);
+        }
+
+        public void InsertCoin(int coin)
+        { 
+            _balance += coin;
+        }
+        public int ReturnChange()
+        {
+            int visszajaro = _balance;
+            _balance = 0;
+            return visszajaro;
+            
+
+        }
+        public Product FindProduct(string code)
+        {
+            foreach (Product product in _products)
+            {
+                if (code == product.Code)
+
+                {
+                    return product;
+                }
+            }
+            return null;  
+        }
+
+
+        public void ListProducts()
+        {
+            foreach (Product product in _products)
+            {
+                if (product.IsAvailable == true)
+                {
+                    Console.WriteLine(product.Code, product.Name, product.Price, product.Category);
+                }
+                else
+                {
+                    Console.WriteLine($"{product.Code},{product.Name}, {product.Price},{product.Category}, ELFOGYOTT");
+                }
+                
+            }
+        }
+
+
+
     }
 }
